d3aa6cd8ab74cba7306f82c9e27e7834567de43f
Michal Gajda <47522674+mgajda83@users.noreply.github.com>
2021-11-14 18:06:56 +0000

    Update Set-KeyVaultSecret.ps1

--- AzureFunctionApp/Set-KeyVaultSecret.ps1
+++ AzureFunctionApp/Set-KeyVaultSecret.ps1
@@ -32,10 +32,75 @@ $Body | Add-Member -NotePropertyName value -NotePropertyValue "$Password"
 $Body = $Body | ConvertTo-Json
 
 #Send Request
-$KeyVaultName = "KeyVault-LAPS-PSMVP"
+$KeyVaultName = "<KeyVault Name>"
 $VaultSecretUri = "https://$KeyVaultName.vault.azure.net/secrets/$($Request.Body.KeyName)/?api-version=2016-10-01"
 
 $null = Invoke-RestMethod -Method PUT -Body $Body -Uri $VaultSecretUri -ContentType 'application/json' -Headers $AuthHeader -ErrorAction Stop
+
+#Send event to Log Analytics
+$WorkspaceID= "<Workspace ID>"  
+$SharedKey = "<Shared Key>"
+$LogType = "LAPSEvents"
+
+$json = @"
+{  "UserPrincipalName": "SYSTEM",
+    "ComputerName": "$($Request.Body.KeyName)",
+    "Action": "LAPSSet",
+    "CreatedOn": "$(Get-Date -Format 'yyyy-MM-ddTHH:mm:ss.000Z')",
+    "Result": "Success",
+    "Id": ""
+}
+"@
+
+# Create the function to create the authorization signature
+Function Build-Signature ($WorkspaceID, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
+{
+    $xHeaders = "x-ms-date:" + $date
+    $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+
+    $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+    $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+    $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+    $sha256.Key = $keyBytes
+    $calculatedHash = $sha256.ComputeHash($bytesToHash)
+    $encodedHash = [Convert]::ToBase64String($calculatedHash)
+    $authorization = 'SharedKey {0}:{1}' -f $WorkspaceID,$encodedHash
+    return $authorization
+}
+
+# Create the function to create and post the request
+Function Post-LogAnalyticsData($WorkspaceID, $sharedKey, $body, $logType)
+{
+    $method = "POST"
+    $contentType = "application/json"
+    $resource = "/api/logs"
+    $rfc1123date = [DateTime]::UtcNow.ToString("r")
+    $contentLength = $body.Length
+    $signature = Build-Signature `
+        -WorkspaceID $WorkspaceID `
+        -sharedKey $sharedKey `
+        -date $rfc1123date `
+        -contentLength $contentLength `
+        -method $method `
+        -contentType $contentType `
+        -resource $resource
+    $uri = "https://" + $WorkspaceID + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    $headers = @{
+        "Authorization" = $signature;
+        "Log-Type" = $logType;
+        "x-ms-date" = $rfc1123date;
+        "time-generated-field" = "";
+    }
+
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    return $response.StatusCode
+
+}
+
+# Submit the data to the API endpoint
+Post-LogAnalyticsData -WorkspaceID $WorkspaceID -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
 
 #Return Password to workstation
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
